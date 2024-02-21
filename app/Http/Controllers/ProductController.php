3abd3438e854722da10efae50c86f777fea90f11
Kirsten A. Valencia <candykaye87@gmail.com>
2024-02-21 12:33:42 +0800

    Initial Commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function show()
+    {
+            return view('Movies')
+                ->with('Movie1, The Greatest Show')
+                ->with('Movie2, KungFu Panda')
+                ->with('Movie3, The Amazing Spiderman')
+                ->with('Movie4, Into The Woods')
+                ->with('Movie5, The Internship');
+            
+    }
+}
+?>
