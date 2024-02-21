7deb357d1bc5d8a624fb32dd1cb8fce87eeea032
Kirsten A. Valencia <candykaye87@gmail.com>
2024-02-21 12:53:10 +0800

    Final Commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,11 +9,11 @@ class ProductController extends Controller
     public function show()
     {
             return view('Product')
-                ->with('Movie1, The Greatest Show')
-                ->with('Movie2, KungFu Panda')
-                ->with('Movie3, The Amazing Spiderman')
-                ->with('Movie4, Into The Woods')
-                ->with('Movie5, The Internship');
+                ->with('Movie1', 'The Greatest Show')
+                ->with('Movie2', 'KungFu Panda')
+                ->with('Movie3', 'The Amazing Spiderman')
+                ->with('Movie4', 'Into The Woods')
+                ->with('Movie5', 'The Internship');
             
     }
 }
